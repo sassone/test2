46c77be8d5638122d5efa0575ff282d4d635cbfc
sassone19 <ciaccias@gmail.com>
2017-04-03 21:38:59 +0100

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`,
+})
+export class AppComponent  {
+        name = 'Angular';
+        title = 'Tour of Heroes';
+        heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+        myHero = this.heroes[0];
+}
